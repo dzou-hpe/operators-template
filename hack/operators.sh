89825ce6ed2875eb25aac0a93406d19c95f35eaa
Di Zou <di.zou@hpe.com>
2022-06-29 08:53:20 -0400

    update

--- hack/operators.sh
+++ hack/operators.sh
@@ -6,20 +6,25 @@ fi
 
 OPERATOR_NAME="${1}"
 
+if [ -d operators/"${OPERATOR_NAME}" ];then
+  echo "Operator: ${OPERATOR_NAME} exists"
+  exit 1
+fi
+
 echo "Create github actions"
 mkdir -p .github/workflows
 cp hack/ci.template.yaml .github/workflows/temp.yaml
-sed "s/@REPLACEME@/${OPERATOR_NAME}/g" .github/workflows/temp.yaml > .github/workflows/${OPERATOR_NAME}-operator.yaml
+sed "s/@REPLACEME@/${OPERATOR_NAME}/g" .github/workflows/temp.yaml > .github/workflows/"${OPERATOR_NAME}"-operator.yaml
 rm .github/workflows/temp.yaml
 
 echo "create with kubebuilder"
-curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)
+curl -L -o kubebuilder "https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)"
 chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
 GIT_REPO=$(cat .git/config  | grep url | awk -F'@' '{print $2}' | awk -F':' '{print $2}' | awk -F'.' '{print $1}')
 cwd=$(pwd)
-mkdir -p operators/${OPERATOR_NAME}
-cd operators/${OPERATOR_NAME}
-kubebuilder init --domain my.domain --repo github.com/${GIT_REPO}/${OPERATOR_NAME}
+mkdir -p operators/"${OPERATOR_NAME}"
+cd operators/"${OPERATOR_NAME}" || exit
+kubebuilder init --domain my.domain --repo github.com/"${GIT_REPO}"/"${OPERATOR_NAME}"
 
 cp Makefile Makefile.tmp
 sed  -e "s/controller:latest/${OPERATOR_NAME}-operator:latest/g"  \
@@ -27,4 +32,4 @@ sed  -e "s/controller:latest/${OPERATOR_NAME}-operator:latest/g"  \
 rm Makefile.tmp
 cat ${cwd}/hack/makefile.patch >> Makefile
 
-cd ${cwd}
+cd "${cwd}" || exit
